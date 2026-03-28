6e22476061f478684cbf5c6cf2bd5eb862f0b3a8
xpayn3 <luka.grcar@me.com>
2026-03-28 14:29:56 +0100

    bump hash

--- app.js
+++ app.js
@@ -185,7 +185,7 @@ if ('serviceWorker' in navigator) {
 }
 
 // Populate version footer + splash version + git hash
-const BUILD_HASH = '8523061';
+const BUILD_HASH = 'ef14921';
 (function() {
   const footer = document.getElementById('appVersionFooter');
   function setVersion(ver) {
